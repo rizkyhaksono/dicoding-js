b42290408ed789f6e59d0772830203e631ba6731
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-06-28 20:04:33 +0700

    add: ES6 function'

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-console.log("hai");
+// cara baru ES6
+import coffeeStock from "./state.js";
 
-console.log("apa kabar?");
+// cara lama
+// const {coffeeStock} = require('./state');
+
+// const makeCoffee = (type, miligram) => {
+//     if (coffeeStock[type] >= miligram) {
+//         console.log("Kopi berhasil dibuat");
+//     } else {
+//         console.log("Kopi gagal dibuat");
+//     }
+// }
+
+// makeCoffee("robusta", 100);
+
+const displayStock = stock => {
+  for (const type in stock) {
+    console.log(type);
+  }
+}
+   
+
+console.log(displayStock(coffeeStock));
+
+console.log(coffeeStock.isMakeCoffee);
+console.log(coffeeStock.coffeeStock);
+
+const json = '{ bad json }';
+ 
+try {
+  const user = JSON.parse(json);
+ 
+  console.log(user.name);
+  console.log(user.age);
+} catch (error) {
+  console.log(error.name);
+  console.log(error.message);
+}
---
+++ state.js
@@ -0,0 +1,9 @@
+const coffeeStock = {
+    arabica: 100,
+    robusta: 150,
+    liberica: 200
+}
+
+const isMakeCoffee = false;
+
+export default {coffeeStock, isMakeCoffee};
