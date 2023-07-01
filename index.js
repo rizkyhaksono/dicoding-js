65a5067f5c816962f671e1530c89a9dcdc6644f1
rizkyhaksono <mrizkyhaksono@gmail.com>
2023-07-02 05:18:24 +0700

    add: promise using promify

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // cara baru ES6
 import coffeeStock from "./state.js";
 
+import { promisify } from "util";
+
 // cara lama
 // const {coffeeStock} = require('./state');
 
@@ -37,3 +39,51 @@ try {
   console.log(error.name);
   console.log(error.message);
 }
+
+// promise function
+// function getUsers(isOffline) {
+//   // return a promise object
+//   return new Promise((resolve, reject) => {
+
+//     // simulate network delay
+//     setTimeout(() => {
+//       const users = ['John', 'Jack', 'Abigail'];
+    
+//       if (isOffline) {
+//         reject(new Error('cannot retrieve users due offline'));
+//         return;
+//       }
+
+//       resolve(users);
+//     }, 3000);
+//   });
+// }
+
+// getUsers(false)
+//   .then(users => console.log(users))
+//   .catch(err => console.log(err.message));
+ 
+// promify from node.js
+function getUsers(isOffline, callback) {
+  // simulate network delay
+  setTimeout(() => {
+    const users = ['John', 'Jack', 'Abigail'];
+     if (isOffline) {
+      callback(new Error('cannot retrieve users due offline'), null);
+      return;
+    }
+ 
+    callback(null, users);
+  }, 3000);
+}
+ 
+// create a Promise version of getUsers
+const getUsersPromise = promisify(getUsers);
+
+getUsersPromise(false)
+  .then(users => console.log(users))
+  .catch(err => console.log(err.message));
+ 
+getUsersPromise(true)
+  .then(users => console.log(users))
+  .catch(err => console.log(err.message)); 
